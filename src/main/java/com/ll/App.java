b242174a40977cff749c8aa876376de864688ac2
N1Alice <kc700370@gmail.com>
2023-11-08 11:51:18 +0900

    2단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,29 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
+    static Scanner scanner = new Scanner(System.in);
     public void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
+
+        while(true){
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("등록")){
+                System.out.print("명언 : ");
+                String WS = scanner.nextLine();
+                System.out.print("작가 : ");
+                String A = scanner.nextLine();
+
+                continue;
+            }
+
+            if (cmd.equals("종료")){
+                break;
+            }
+
+        }
     }
 }
